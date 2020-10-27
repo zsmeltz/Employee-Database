1127e09e1cb15c9e6d9d249fec89dc97c595f226
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-10-26 21:18:13 -0500

    Now that I got a basis down in the sql file, I am now working on the db.js file creating prompts. The first one I thought of was a options prompt that brings up a menu of things to do like view employees, add employees, remove them, update them, etc. This came from the GIF that was sent as an asset so I took that idea and started off with it to get a basis of what all the functionality entails

--- db.js
+++ db.js
@@ -2,22 +2,40 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var consoleTable = require('console.table');
 
-var connection = mysql.createConnection({
-    host: "localhost",
+// var connection = mysql.createConnection({
+//     host: "localhost",
   
-    // Your port; if not 3306
-    port: 3500,
+//     // Your port; if not 3306
+//     port: 3500,
   
-    // Your username
-    user: "root",
+//     // Your username
+//     user: "root",
   
-    // Your password
-    password: "root",
-    database: "employeesDB"
-  });
+//     // Your password
+//     password: "root",
+//     database: "employeesDB"
+//   });
 
-  connection.connect(function(err){
-      if(err) throw err;
-      console.log("Connected as id " + connection.threadId + "\n");
-  });
+//   connection.connect(function(err){
+//       if(err) throw err;
+//       console.log("Connected as id " + connection.threadId + "\n");
+//   });
 
+initialPrompt();
+
+function initialPrompt(){
+    var options = {
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager", "View all roles", "View all departments", "Add role", "Remove role", "EXIT"],
+        name: "Option Menu"
+    };
+    inquirer.prompt(options).then(function(res, err) {
+        if(err) throw err;
+
+        if(res === options.choices[11]){
+            console.clear();
+        }
+        
+    })
+}
