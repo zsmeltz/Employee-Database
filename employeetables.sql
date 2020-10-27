60dccd71a25ae99bd75424d2c73ec30093f1d053
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-10-26 20:00:45 -0500

    created three tables in the sql file named department, emp_role, and employee. Each hold their own IDs that auto increment each time you add a new key.

--- employeetables.sql
+++ employeetables.sql
@@ -4,3 +4,26 @@ CREATE DATABASE employeesDB;
 
 USE employeesDB;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    dep_name VARCHAR(30) NULL
+);
+
+CREATE TABLE emp_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,2) NULL,
+    dept_id INT NOT NULL,
+    FOREIGN KEY (dep_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES emp_role(id)
+);
