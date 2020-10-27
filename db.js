b776d7c8adc7d4d30bcb71dce6a71ecc83af6786
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-10-26 20:09:10 -0500

    Added a variable connection that creates a  connection to the mysql db, set the port to 3500 and the user as 'root'

--- db.js
+++ db.js
@@ -1,2 +1,21 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3500,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "employeesDB"
+  });
+
+  connection.connect(function(err){
+      if(err) throw err;
+      console.log("Connected as id " + connection.threadId + "\n");
+  })
